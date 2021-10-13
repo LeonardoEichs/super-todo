e0022b28a63820555a938a44a81880c37e7a7d54
Leonardo Eichstaedt <leonardoeichs@gmail.com>
2021-10-13 03:28:47 -0300

    reorganize routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import React from "react";
-import Routes from "./Routes";
 import GlobalStyle from "./styles/globalStyles";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Login from "pages/Login";
+import Registration from "pages/Registration";
+import ToDo from "pages/ToDo";
 
 function App() {
   return (
     <div className="app">
       <GlobalStyle />
-      <Routes />
+      <Router>
+        <Switch>
+          <Redirect exact from="/" to="/login" />
+          <Route path="/login" component={Login} />
+          <Route path="/registration" component={Registration} />
+          <Route path="/todo" component={ToDo} />
+        </Switch>
+      </Router>
     </div>
   );
 }
