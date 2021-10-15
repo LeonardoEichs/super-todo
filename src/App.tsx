68d5a5c811c369105a80577166fcdf75acb29312
Leonardo Eichstaedt <leonardoeichs@gmail.com>
2021-10-14 21:24:12 -0300

    add protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,11 @@
 import React from "react";
 import GlobalStyle from "./styles/globalStyles";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from "react-router-dom";
-import Login from "pages/Login";
-import Registration from "pages/Registration";
-import ToDo from "pages/ToDo";
-import SingleToDo from "pages/ToDo/SingleToDo";
-import { AuthProvider } from "contexts/auth.context";
-
+import Routes from "Routes";
 function App() {
   return (
     <div className="app">
       <GlobalStyle />
-      <Router>
-        <AuthProvider>
-          <Switch>
-            <Redirect exact from="/" to="/login" />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/registration" component={Registration} />
-            <Route exact path="/todo" component={ToDo} />
-            <Route exact path="/todo/:id" component={SingleToDo} />
-          </Switch>
-        </AuthProvider>
-      </Router>
+      <Routes />
     </div>
   );
 }
---
+++ src/Routes.tsx
@@ -0,0 +1,43 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { AuthProvider, AuthContext } from "contexts/auth.context";
+import { useContext } from "react";
+
+import Login from "pages/Login";
+import Registration from "pages/Registration";
+import ToDo from "pages/ToDo";
+import SingleToDo from "pages/ToDo/SingleToDo";
+
+interface IPrivateRoute {
+  [key: string]: any;
+}
+
+const PrivateRoute = ({ ...rest }: IPrivateRoute) => {
+  const { authState } = useContext(AuthContext);
+
+  if (authState.id == "") {
+    return <Redirect to="/login" />;
+  }
+
+  return <Route {...rest} />;
+};
+
+const Routes = () => (
+  <Router>
+    <AuthProvider>
+      <Switch>
+        <Redirect exact from="/" to="/login" />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/registration" component={Registration} />
+        <PrivateRoute exact path="/todo" component={ToDo} />
+        <PrivateRoute exact path="/todo/:id" component={SingleToDo} />
+      </Switch>
+    </AuthProvider>
+  </Router>
+);
+
+export default Routes;
